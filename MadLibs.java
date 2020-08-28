30ff15a3459bdbfeee76150bd174b59f04831b12
emerygenga <70218311+emerygenga@users.noreply.github.com>
2020-08-27 19:12:59 -0700

    Part 1
    This uses a scanner to read in a file titled "nouns.txt" and stores them in an ArrayList titled nouns

--- MadLibs.java
+++ MadLibs.java
@@ -1,11 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MadLibs {
 
-	public static void main(String[] args) {
 	public static void main(String[] args) throws FileNotFoundException {
 		//0. Give variable names for each of the arraylists below and create Madlibs stories.
 		
-		//helloooooo
+		//1. Read a nouns.txt file and store its list of nouns into an arraylist 
+		
+		File nounsFile = new File ("nouns.txt"); 
+		Scanner nounScanner = new Scanner (nounsFile); 
+		ArrayList<String> nouns = new ArrayList<String>(); 
+		while (nounScanner.hasNextLine())
+		{
+			nouns.add(nounScanner.nextLine()); 
+		}
+		
 		//2. Read a verbs.txt file and store its list of verbs into an arraylist.
 		
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
@@ -15,7 +27,7 @@ public class MadLibs {
 		//5. Prompt user for the name of a file containing a MadLibs story.
 		// Read the story from that file and write it to System.out, but replacing each instance of
 		// <<NOUN>>, <<VERB>>, etc. with a randomly chosen word from the corresponding list.
-		System.out.println("this is a test");
+		
 	}
 
 }
