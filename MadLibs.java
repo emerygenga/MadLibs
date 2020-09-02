2c55f7cf69154a74777eafd4ebd5bdaa39e4445d
Sally Ho <sallyho123@yahoo.com>
2020-09-01 23:45:29 -0700

    added throw exception

--- MadLibs.java
+++ MadLibs.java
@@ -9,14 +9,19 @@ public class MadLibs
 		ArrayList <String> verbs = new ArrayList <String>();
 		//1. Read a nouns.txt file and store its list of nouns into an arraylist. 
 		//2. Read a verbs.txt file and store its list of verbs into an arraylist.
-		FileReader fr = new FileReader ("verbs.txt");
-		BufferedReader br = new BufferedReader(fr);
-		while (br.ready())
-		{
-			verbs.add(br.readLine());
+		try {
+			FileReader fr = new FileReader ("verbs.txt");
+			BufferedReader br = new BufferedReader(fr);
+			while (br.ready())
+			{
+				verbs.add(br.readLine());
+			}
+			br.close();
+			fr.close();
 		}
-		br.close();
-		fr.close();
+		catch (Exception exe) {
+			System.out.println ("The verbs.txt file did not work correctly");
+		}
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
 		//4. Read an adverbs.txt file and store its list of adverbs into an arraylist.
 		//5. Prompt user for the name of a file containing a MadLibs story.
