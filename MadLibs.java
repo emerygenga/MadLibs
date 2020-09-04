e6a880e702f7037c72b2a747712eb874e1773002
annelieselovespugs <annelieseardizzone@Brynhildr.local>
2020-09-03 22:02:41 -0700

    Updated version of step 5
    This now includes everyone's code. I hope I am commiting this to the correct place.

--- MadLibs.java
+++ MadLibs.java
@@ -6,33 +6,80 @@ public class MadLibs {
 
 	public static void main(String[] args) {
 		//0. Give variable names for each of the arraylists below and create Madlibs stories.
-		
+		ArrayList <String> nouns = new ArrayList <String>(); 
+		ArrayList <String> verbs = new ArrayList <String> ();
+		ArrayList <String> adj = new ArrayList<String>();
+		ArrayList <String> adverbList = new ArrayList<String>(); 
+
 		//1. Read a nouns.txt file and store its list of nouns into an arraylist.
-		//helloooooo
-		//2. Read a verbs.txt file and store its list of verbs into an arraylist.
+		try {
+			File nounsFile = new File ("nouns.txt"); 
+			Scanner nounScanner = new Scanner (nounsFile); 
+			while (nounScanner.hasNextLine())
+			{
+				nouns.add(nounScanner.nextLine()); 
+			}
+		}
+		catch (Exception exe) {
+			System.out.println ("The nouns.txt file did not work correctly");
+		}
 		
+
+		
+		//2. Read a verbs.txt file and store its list of verbs into an arraylist.
+		try {
+			FileReader fr = new FileReader ("verbs.txt");
+			BufferedReader br = new BufferedReader(fr);
+			while (br.ready())
+			{
+				verbs.add(br.readLine());
+			}
+			br.close();
+			fr.close();
+		}
+		catch (Exception exe) {
+			System.out.println ("The verbs.txt file did not work correctly");
+		}
+
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
+		try {
+			FileReader fReader = new FileReader ("adjectives.txt");
+			BufferedReader bReader = new BufferedReader (fReader);
+			String str = bReader.readLine();
+			while (str!=null) {
+				adj.add(str);
+				str = bReader.readLine();
+			}
+			fReader.close();
+			bReader.close();
+		}
+		catch (Exception exe) {
+			System.out.println ("The adjectives.txt file did not work correctly");
+		}
 		
 		//4. Read an adverbs.txt file and store its list of adverbs into an arraylist.
+		String line = null;
+		try {
+			FileReader fr = new FileReader ("adverbs.txt");
+			BufferedReader br = new BufferedReader(fr);
+			while ((line = br.readLine()) != null) {
+				adverbList.add(line);
+			}
+			br.close();
+			fr.close();
+		}
+		catch (Exception e) {
+			System.out.println ("Error Occurred");
+		}
+
 
 		//5. Prompt user for the name of a file containing a MadLibs story.
 		// Read the story from that file and write it to System.out, but replacing each instance of
 		// <<NOUN>>, <<VERB>>, etc. with a randomly chosen word from the corresponding list.
     	Scanner keyboard = new Scanner(System.in);
-
     	//Getting the story's file name from the user
     	System.out.print ("Please give me the name of a MadLibs file and press enter: ");
     	String madLibsFileName = keyboard.nextLine();
-    	//The below code checks to see if the user inputted a valid name. It might not be necessary. There are bugs. 
-    	/*while((madLibsFileName.length()<4) || ((madLibsFileName.substring(madLibsFileName.length()-4,madLibsFileName.length())) != ".txt")){
-    		System.out.println("thing inputted: " + madLibsFileName + " | length: " + madLibsFileName.length() + " | substring: " + madLibsFileName.substring(madLibsFileName.length()-4,madLibsFileName.length()));
-    		System.out.println ((madLibsFileName.substring(madLibsFileName.length()-4,madLibsFileName.length())) == ".txt");
-    		madLibsFileName = "";
-    		System.out.println("Invalid. The file must be a .txt file.\nPlease give me the name of a MadLibs file: ");
-    		madLibsFileName = keyboard.next();
-    	}*/
-
-
     	//Writing the story character by character to a string
 		String story = ""; //This is the string that contains the final MadLib file that we are writing to System.out
 		try{
@@ -48,31 +95,24 @@ public class MadLibs {
 		catch(IOException ex){
 			System.out.println ("Something went wrong. :(");
 		}
-		
-
-
 		//update the variable names for each list!!!!!!! *****
 		//check to see if there are any words that need to be added and add them when needed, then rinse and repeat for every other type of word
-		//nouns (listOfNouns)
+		//nouns (nouns)
 		while (story.indexOf("<<NOUN>>") > -1){
-			story = story.substring(0,(story.indexOf("<<NOUN>>"))) + listOfNouns.get((int)(Math.random()*listOfNouns.size())) + story.substring((story.indexOf("<<NOUN>>"))+8,story.length());
+			story = story.substring(0,(story.indexOf("<<NOUN>>"))) + nouns.get((int)(Math.random()*nouns.size())) + story.substring((story.indexOf("<<NOUN>>"))+8,story.length());
 		}
-
-		//verbs (listOfVerbs)
+		//verbs (verbs)
 		while (story.indexOf("<<VERB>>") > -1){
-			story = story.substring(0,(story.indexOf("<<VERB>>"))) + listOfVerbs.get((int)(Math.random()*listOfVerbs.size())) + story.substring((story.indexOf("<<VERB>>"))+8,story.length());
+			story = story.substring(0,(story.indexOf("<<VERB>>"))) + verbs.get((int)(Math.random()*verbs.size())) + story.substring((story.indexOf("<<VERB>>"))+8,story.length());
 		}
-
-		//adjectives (listOfAdjs)
+		//adjectives (adj)
 		while (story.indexOf("<<ADJECTIVE>>") > -1){
-			story = story.substring(0,(story.indexOf("<<ADJECTIVE>>"))) + listOfAdjs.get((int)(Math.random()*listOfAdjs.size())) + story.substring((story.indexOf("<<ADJECTIVE>>"))+13,story.length());
+			story = story.substring(0,(story.indexOf("<<ADJECTIVE>>"))) + adj.get((int)(Math.random()*adj.size())) + story.substring((story.indexOf("<<ADJECTIVE>>"))+13,story.length());
 		}
-
-		//adverbs (listofAdvs)
+		//adverbs (adverbList)
 		while (story.indexOf("<<ADVERB>>") > -1){
-			story = story.substring(0,(story.indexOf("<<ADVERB>>"))) + listOfAdvs.get((int)(Math.random()*listOfAdvs.size())) + story.substring((story.indexOf("<<ADVERB>>"))+10,story.length());
+			story = story.substring(0,(story.indexOf("<<ADVERB>>"))) + adverbList.get((int)(Math.random()*adverbList.size())) + story.substring((story.indexOf("<<ADVERB>>"))+10,story.length());
 		}
-
 		//write to System.out
 		System.out.println(story);
 	}
